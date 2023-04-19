d882d0fcb13787f661aa262b342e1a04a0ae5b77
biangomes <beanascigom@gmail.com>
2023-04-19 15:02:54 -0300

    findById() method implemented in the WordServiceTest

--- src/test/java/com/example/datahubteste/service/WordServiceTest.java
+++ src/test/java/com/example/datahubteste/service/WordServiceTest.java
@@ -3,6 +3,7 @@ package com.example.datahubteste.service;
 import com.example.datahubteste.ApplicationConfigTest;
 import com.example.datahubteste.model.Word;
 import com.example.datahubteste.repository.WordRepository;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
@@ -11,6 +12,11 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.BDDMockito.given;
 
 @DisplayName("WordServiceTest")
 public class WordServiceTest extends ApplicationConfigTest {
@@ -29,5 +35,19 @@ public class WordServiceTest extends ApplicationConfigTest {
         Mockito.when(repo.findAll()).thenReturn(records);
 
         service.findAll();
+    }
+
+
+    @Test
+    @DisplayName("Deve retornar uma word por ID")
+    public void deve_retornar_uma_word_por_id() throws Throwable {
+        Word word = new Word();
+        word.setId(UUID.randomUUID());
+
+        given(repo.findById(word.getId())).willReturn(Optional.of(word));
+        final Optional<Word> expectedWord = Optional.ofNullable(service.findById(word.getId()));
+
+        Assertions.assertThat(expectedWord).isNotNull();
+
     }
 }
