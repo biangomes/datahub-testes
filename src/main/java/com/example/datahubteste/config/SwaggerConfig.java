13ab8c8df0a6c74af1e6f47dd24f6022843c2d95
biangomes <beanascigom@gmail.com>
2023-04-23 10:01:48 -0300

    Pacote de configuração e classe SwaggerConfig, de configuração do Swagger, adicionados e implementados

---
+++ src/main/java/com/example/datahubteste/config/SwaggerConfig.java
@@ -0,0 +1,44 @@
+package com.example.datahubteste.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.service.VendorExtension;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.ArrayList;
+
+import static springfox.documentation.builders.PathSelectors.regex;
+
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig {
+
+    @Bean
+    public Docket wordApi() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.example.datahubtest"))
+                .paths(regex("/api.*"))
+                .build()
+                .apiInfo(metaInfo());
+    }
+
+    private ApiInfo metaInfo() {
+        ApiInfo apiInfo = new ApiInfo(
+                "Word API REST",
+                "API Rest que persiste uma palavra e a retorna invertida",
+                "1.0",
+                "Terms of Service",
+                new Contact("Beatriz Gomes", "https://github.com/biangomes", "beanascigom@gmail.com"),
+                "Apache License Version 2.0",
+                "https://www.apache.org/license.html", new ArrayList<VendorExtension>()
+        );
+
+        return apiInfo;
+    }
+}
