65a60df17eab32b981d80ad53fa60096223beee3
biangomes <beanascigom@gmail.com>
2023-04-24 17:17:46 -0300

    OpenAPI criadas, bem como dockerfile e docker-compose

--- src/main/java/com/example/datahubteste/controller/WordController.java
+++ src/main/java/com/example/datahubteste/controller/WordController.java
@@ -2,8 +2,6 @@ package com.example.datahubteste.controller;
 
 import com.example.datahubteste.model.Word;
 import com.example.datahubteste.service.WordService;
-import io.swagger.annotations.Api;
-import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -14,15 +12,13 @@ import java.util.Optional;
 import java.util.UUID;
 
 @RestController
-@RequestMapping("/")
-@Api(value="Word REST API")
+@RequestMapping("/word")
 public class WordController {
 
     @Autowired
     private WordService service;
 
-    @PostMapping("")
-    @ApiOperation(value="Save an object of Word type through param")
+    @PostMapping("/save")
     public Word word(@RequestBody Word word) {
         Word record = service.save(word);
         String invertedWord = service.invertWord(word.getWord());
@@ -31,15 +27,15 @@ public class WordController {
 
 
     @GetMapping("/")
-    @ApiOperation(value="Return a list of Words stored in the database")
     public ResponseEntity<List<Word>> getAll() {
         List<Word> records = service.findAll();
         return new ResponseEntity<>(records, HttpStatus.OK);
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<Word> getById(@PathVariable("id") Long id) throws Throwable {
-        Word record = service.findById(id);
+    public ResponseEntity<Word> getById(@PathVariable("id") String id) throws Throwable {
+        Long longId = Long.parseLong(id);
+        Word record = service.findById(longId);
         return new ResponseEntity<>(record, HttpStatus.OK);
     }
 
